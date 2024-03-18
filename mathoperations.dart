6e8292156af58c19268826d05a174d9a66770011
beni47 <njugunaben007@gmail.com>
2024-03-18 12:44:45 +0300

    test

---
+++ mathoperations.dart
@@ -0,0 +1,23 @@
+//Write a Dart program that performs two mathematical operations using functions
+void main() {
+  int num1 = 15;
+  int num2 = 563;
+
+  // Perform addition
+  int sum = add(num1, num2);
+  print("The sum of $num1 and $num2 is $sum");
+
+  // Perform multiplication
+  int product = multiply(num1, num2);
+  print("The product of $num1 and $num2 is $product");
+}
+
+// Function to add two numbers
+int add(int a, int b) {
+  return a + b;
+}
+
+// Function to multiply two numbers
+int multiply(int a, int b) {
+  return a * b;
+}
---
+++ personalinfo.dart
@@ -0,0 +1,11 @@
+//Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+
+void main() {
+  String name = "Benny";
+  int age = 23;
+  String school = "PLP Academy";
+  String hobby = "Playing football, programming and stand-up comedy";
+
+  print(
+      "My name is $name. I am $age years old. I attend $school, and my hobby include $hobby.");
+}
